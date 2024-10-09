3edbfa9d0ef9a6e1efce3e81aa0859c38293808f
Kozhagulov Ramazan <kozhagulov.r@phystech.edu>
2024-10-09 18:45:47 +0300

    Split GameScene by managers

--- client/src/objects/enemies/GoblinEnemy.js
+++ client/src/objects/enemies/GoblinEnemy.js
@@ -2,7 +2,7 @@ import Phaser from 'phaser';
 import Enemy from './Enemy';
 
 class GoblinEnemy extends Enemy {
-    constructor(scene, x, y, texture) {
+    constructor(scene, x, y, texture, cost = 100) {
         super(scene, x, y, texture);
         this.speed = 150;
         this.health = 80; // Меньший запас здоровья
